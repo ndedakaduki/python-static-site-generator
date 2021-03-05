dedb776d1c057605db247caa8dc4df44e174de0f
Ndeda Kaduki <kadukindeda@gmail.com>
2021-03-05 12:59:52 +0300

    Completed module

--- ssg.py
+++ ssg.py
@@ -6,9 +6,15 @@ from ssg.site import Site
 
 def main(source='content', dest='dist'):
     config = {
-        'source': source,
-        'dest': dest,
-        'parsers': [ssg.parsers.ResourceParser()]
+        'source':
+            source,
+        'dest':
+            dest,
+        'parsers': [
+            ssg.parsers.ResourceParser(),
+            ssg.parsers.MarkdownParser(),
+            ssg.parsers.ReStructuredTextParser(),
+        ],
     }
     Site(**config).build()
 
